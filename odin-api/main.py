18112ff86c611cca001ce642c94279dd79c6f4a0
shoolpani dubey <shool.pani.dubey@gmail.com>
2023-08-31 22:17:11 +0300

    Overlay a SAR image to the map - Backend modified to server odin-api/SAR_image_20420212.png - Image served from static directory

--- odin-api/main.py
+++ odin-api/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+# Serving the image as static file
+app.mount("/odin-api", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
 async def root():
-    return "ODIN-API NOT FOUND"
+    return "ODIN-API IS UP."
